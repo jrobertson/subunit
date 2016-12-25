414d1299ab91828cf38a003713904aa41d89025f
James Robertson <james@r0bertson.co.uk>
2016-12-25 11:45:12 +0000

    revision for Ruby 2.4: Replaced Fixnum with Integer

--- lib/subunit.rb
+++ lib/subunit.rb
@@ -3,30 +3,45 @@
 
 class Subunit
 
-  def initialize(raw_units=[], subunit=0)
-    units = multiply_units(raw_units.clone).reverse
-    @a = scan_units(units, subunit)    
-  end
-
-  def to_a()
-    @a.map{|x| x >= 1 ? x : 0}
+  attr_reader :to_a, :to_h
+  
+  def initialize(raw_units=nil, raw_subunit=0)
+    method((raw_units.class.to_s.downcase + '_units').to_sym).call(raw_units, raw_subunit)
   end
 
   private
 
-  def multiply_units(x)
-    if x.length > 1 then
-      y, k = x.shift 2
-      r = y.to_i * k.to_i
-      [r] + multiply_units([r] + x)
-    else
-      []
-    end
+  def hash_subunit(h)
+    [h.values.first, h.keys.first]
   end
 
-  def scan_units(unit_list,seconds)
+  def fixnum_subunit(v)
+    [v, :remainder]
+  end
+  
+  def integer_subunit(v)
+    [v, :remainder]
+  end  
+  
+  def hash_units(raw_units={}, u=nil)
+    val, label = method((u.class.to_s.downcase + '_subunit').to_sym).call(u)
+    units = multiply_units(raw_units.values).reverse
+    @to_a = scan_units(units, val).map(&:to_i)
+    @to_h = Hash[(raw_units.keys.reverse + [label]).zip(@to_a)]
+  end
+
+  def array_units(raw_units=[], u=nil)
+    val, label = method((u.class.to_s.downcase + '_subunit').to_sym).call(u)
+    units = multiply_units(raw_units).reverse
+    @to_a = scan_units(units, val).map(&:to_i)
+  end
+
+  def multiply_units(x) x.inject([]){|r,x| r + [x.to_i * (r[-1] || 1)]} end
+
+  def scan_units(unit_list,raw_subunit)
     n = unit_list.shift    
-    unit, subunit = [:/,:%].map{|x| seconds.send x, n}  
+    unit, subunit = [:/,:%].map{|x| raw_subunit.send x, n}  
     unit_list.length > 0 ? [unit] + scan_units(unit_list, subunit) : [unit, subunit]
   end
 end
+
